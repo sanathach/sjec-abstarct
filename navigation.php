3bcc12a0b618fc63d9309424a8e11c4f7bcb8386
geethu <geetheshbu302@gmail.com>
2023-06-14 12:19:49 +0530

    initail commit

---
+++ navigation.php
@@ -0,0 +1,61 @@
+<header class="short-header">   
+
+   	<div class="gradient-block"></div>	
+
+   	<div class="row header-content">
+
+   		<div class="logo">
+	         <a href="index.html">Author</a>
+	      </div>
+
+	   	<nav id="main-nav-wrap">
+				<ul class="main-navigation sf-menu">
+					<li class="current"><a href="index.php" title="">Home</a></li>									
+					<li class="has-children">
+						<a href="category.php" title="">Departments</a>
+						<ul class="sub-menu">
+			            <li><a href="category.php">MCA</a></li>
+			            <li><a href="category.php">MBA</a></li>
+			            <li><a href="category.php">CSE</a></li>
+			            <li><a href="category.php">ME</a></li>
+			            <li><a href="category.php">CIVIL</a></li>
+			            <li><a href="category.php">ECE</a></li>
+			         </ul>
+					</li>
+					<li class="has-children">
+						<a href="single-standard.html" title="">Blog</a>
+						<ul class="sub-menu">
+			            <li><a href="single-video.php">Video Post</a></li>
+			            <li><a href="single-audio.php">Audio Post</a></li>
+			            <li><a href="single-gallery.php">Gallery Post</a></li>
+			            <li><a href="single-standard.php">Standard Post</a></li>
+			         </ul>
+					</li>
+					<li><a href="style-guide.php" title="">Styles</a></li>
+					<li><a href="about.php" title="">About</a></li>	
+					<li><a href="contact.php" title="">Contact</a></li>										
+				</ul>
+			</nav> <!-- end main-nav-wrap -->
+
+			<div class="search-wrap">
+				
+				<form role="search" method="get" class="search-form" action="#">
+					<label>
+						<span class="hide-content">Search for:</span>
+						<input type="search" class="search-field" placeholder="Type Your Keywords" value="" name="s" title="Search for:" autocomplete="off">
+					</label>
+					<input type="submit" class="search-submit" value="Search">
+				</form>
+
+				<a href="#" id="close-search" class="close-btn">Close</a>
+
+			</div> <!-- end search wrap -->	
+
+			<div class="triggers">
+				<a class="search-trigger" href="#"><i class="fa fa-search"></i></a>
+				<a class="menu-toggle" href="#"><span>Menu</span></a>
+			</div> <!-- end triggers -->	
+   		
+   	</div>     		
+   	
+   </header> <!-- end header -->
